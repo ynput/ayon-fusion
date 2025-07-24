b2dbeab523472798794ab76f9096d7b4356422be
Roy Nieterau <roy_nieterau@hotmail.com>
2025-07-24 17:11:15 +0200

    Match more with Maya PR https://github.com/ynput/ayon-maya/pull/311

--- client/ayon_fusion/plugins/publish/increment_current_file.py
+++ client/ayon_fusion/plugins/publish/increment_current_file.py
@@ -1,9 +1,11 @@
+import os
+
 import pyblish.api
 
-from ayon_core.pipeline import OptionalPyblishPluginMixin
+from ayon_core.pipeline import OptionalPyblishPluginMixin, registered_host
 from ayon_core.lib import version_up
+from ayon_core.host import IWorkfileHost
 from ayon_fusion.api import FusionHost
-from ayon_core.pipeline import registered_host
 
 
 class FusionIncrementCurrentFile(
@@ -25,30 +27,39 @@ class FusionIncrementCurrentFile(
         comp = context.data.get("currentComp")
         assert comp, "Must have comp"
 
-        current_filepath = context.data["currentFile"]
-        new_filepath = version_up(current_filepath)
-
+        # Fusion can have multiple compositions open at the same time, and
+        # as a publish is running the user may switch to another comp
+        # simultaneously. Hence, we need to ensure the active comp is the
+        # one current publish session is for in the registered host.
         host: FusionHost = registered_host()
         with host.current_comp(comp):
-            # Fusion can have multiple compositions open at the same time, and
-            # as a publish is running the user may switch to another comp
-            # simultaneously. Hence, we need to ensure the active comp is the
-            # one current publish session is for.
-            if hasattr(host, "save_workfile_with_context"):
-                from ayon_core.host.interfaces import SaveWorkfileOptionalData
-                host.save_workfile_with_context(
-                    filepath=new_filepath,
-                    folder_entity=context.data["folderEntity"],
-                    task_entity=context.data["taskEntity"],
-                    description="Incremented by publishing.",
-                    # Optimize the save by reducing needed queries for context
-                    prepared_data=SaveWorkfileOptionalData(
-                        project_entity=context.data["projectEntity"],
-                        project_settings=context.data["project_settings"],
-                        anatomy=context.data["anatomy"],
-                    )
+            self.increment_workfile(context)
+
+    def increment_workfile(self, context: pyblish.api.Context):
+        """Increment the current workfile version using registered host."""
+        current_filepath: str = context.data["currentFile"]
+        try:
+            from ayon_core.pipeline.workfile import save_next_version
+            from ayon_core.host.interfaces import SaveWorkfileOptionalData
+
+            current_filename = os.path.basename(current_filepath)
+            save_next_version(
+                description=(
+                    f"Incremented by publishing from {current_filename}"
+                ),
+                # Optimize the save by reducing needed queries for context
+                prepared_data=SaveWorkfileOptionalData(
+                    project_entity=context.data["projectEntity"],
+                    project_settings=context.data["project_settings"],
+                    anatomy=context.data["anatomy"],
                 )
-            else:
-                # Backwards compatibility before:
-                # https://github.com/ynput/ayon-core/pull/1275
-                host.save_workfile(new_filepath)
+            )
+        except ImportError:
+            # Backwards compatibility before ayon-core 1.5.0
+            self.log.debug(
+                "Using legacy `version_up`. Update AYON core addon to "
+                "use newer `save_next_version` function."
+            )
+            new_filepath = version_up(current_filepath)
+            host: IWorkfileHost = registered_host()
+            host.save_workfile(new_filepath)
